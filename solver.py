19c0f8887f57dc8ab4853633eb236787c60ee6c0
elsalibymichel <elsalibymichel@gmail.com>
2024-05-27 16:01:07 +0200

    Update: Solver

--- solver.py
+++ solver.py
@@ -15,14 +15,14 @@ class Solver:
             [a for a,_ in enumerate(self.data.arcs)], vtype=gb.GRB.BINARY, name="X_as"
         )
 
-        # Define alpha as a continuous variable
-        self.alpha = model.addVar(vtype=gb.GRB.CONTINUOUS, name="alpha")
+        # Define alpha
+        self.alpha = model.addVar(vtype=gb.GRB.INTEGER, name="alpha")
 
         costs_list = []
         for arc in self.data.arcs:
             cost = 0
             if (arc[0] == "A1") or (arc[0] == "A2"):
-                cost = arc[-1]
+                cost = arc[4] # arc = ("As", job_1, t1, job_2, t2)
             costs_list.append(cost)
 
         costs = np.array(costs_list)
@@ -36,8 +36,8 @@ class Solver:
                 gb.quicksum(
                     self.X_as[index]
                     for index in range(len(self.data.arcs))
-                    if (self.data.arcs[index][0] == "A1" or self.data.arcs[index][0] == "A2")
-                    and self.data.arcs[index][3] == j
+                    if ((self.data.arcs[index][0] == "A1" or self.data.arcs[index][0] == "A2")
+                        and self.data.arcs[index][3] == j)
                 ) == 1
             )
 
@@ -46,43 +46,44 @@ class Solver:
             gb.quicksum(
                 self.X_as[index]
                 for index in range(len(self.data.arcs))
-                if self.data.arcs[index][0] == "A2"
+                if (self.data.arcs[index][0] == "A2")
             ) == 1
         )
 
         # Constraint 3a
         for node in self.data.O:
             job = node[0] # It should be always 0
-            if job != 0:
+            if (job != 0):
                 raise Exception("Error in the node list O")
             time = node[1]
-            if (time == 0) or (time == self.data.T):
+            if ((time == 0) or (time == self.data.T)):
                 continue
             else:
                 model.addConstr(
                     gb.quicksum(
                         self.X_as[index]
                         for index in range(len(self.data.arcs))
-                        if self.data.arcs[index][3] == job
+                        if ((self.data.arcs[index][3] == 0) and (self.data.arcs[index][4] == time)) # job = 0
                     ) - gb.quicksum(
                         self.X_as[index]
                         for index in range(len(self.data.arcs))
-                        if self.data.arcs[index][1] == job
+                        if ((self.data.arcs[index][1] == 0) and (self.data.arcs[index][2] == time)) # job = 0
                     ) == 0
                 )
 
         # Constraint 3b
         for node in self.data.R:
             job = node[0]
+            time = node[1]
             model.addConstr(
                 gb.quicksum(
                     self.X_as[index]
                     for index in range(len(self.data.arcs))
-                    if self.data.arcs[index][3] == job
+                    if ((self.data.arcs[index][3] == job) and (self.data.arcs[index][4] == time))
                 ) - gb.quicksum(
                     self.X_as[index]
                     for index in range(len(self.data.arcs))
-                    if self.data.arcs[index][1] == job
+                    if ((self.data.arcs[index][1] == job) and (self.data.arcs[index][2] == time))
                 ) == 0
             )
 
@@ -91,7 +92,7 @@ class Solver:
             cost_arcs_entering_j = gb.quicksum(
                 costs[index] * self.X_as[index]
                 for index in range(len(self.data.arcs))
-                if (self.data.arcs[index][0] == "A1" or self.data.arcs[index][0] == "A2")
+                if ((self.data.arcs[index][0] == "A1") or (self.data.arcs[index][0] == "A2"))
                 and self.data.arcs[index][3] == j
             )
             model.addConstr(self.alpha >= cost_arcs_entering_j)
@@ -108,11 +109,11 @@ class Solver:
 
 
 
-    def get_solution(self):
+    def get_solution_alpha(self):
         if self.model.status == gb.GRB.OPTIMAL:
             solution = {
-                'alpha': self.alpha.X,
-                'X_as': {index: self.X_as[index].X for index in self.X_as}
+                'alpha': self.alpha.X
+                #'X_as': {index: self.X_as[index].X for index in self.X_as}
             }
             return solution
         else:
